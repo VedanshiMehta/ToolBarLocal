36f03cf9dec09b200360b7d9d722a2d8613713a4
vedanshi.mehta <vedanshi.mehta@1R-76>
2022-01-31 18:29:22 +0530

    Add project files.

---
+++ ToolBarLocal/MainActivity.cs
@@ -0,0 +1,45 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using System;
+
+namespace ToolBarLocal
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        private Button mybutton;
+        private Toolbar myToolbar;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            UIReference();
+            SetActionBar(myToolbar);
+            mybutton.Click += Mybutton_Click;
+        }
+
+        private void UIReference()
+        {
+            mybutton = FindViewById<Button>(Resource.Id.button1);
+            myToolbar = FindViewById<Toolbar>(Resource.Id.toolbar1);
+        }
+
+        private void Mybutton_Click(object sender, System.EventArgs e)
+        {
+            StartActivity(intent: new Android.Content.Intent(this, typeof(SecondActivity)));
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ ToolBarLocal/SecondActivity.cs
@@ -0,0 +1,40 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using AndroidX.AppCompat.App;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Toolbar = AndroidX.AppCompat.Widget.Toolbar;
+
+
+namespace ToolBarLocal
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme")]
+    class SecondActivity: AppCompatActivity
+    {
+
+        Toolbar tool;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.ToolBar);
+            tool = FindViewById<AndroidX.AppCompat.Widget.Toolbar>(Resource.Id.toolbar2);
+            SetSupportActionBar(tool);
+            SupportActionBar.SetDisplayHomeAsUpEnabled(showHomeAsUp: true);
+            tool.NavigationClick += Toolbar_NavigationClick;
+
+        }
+
+        
+
+        private void Toolbar_NavigationClick(object sender, Toolbar.NavigationClickEventArgs e)
+        {
+            Finish();
+        }
+    }
+}
